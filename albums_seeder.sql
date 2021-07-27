021ae56870d7c3bf8fda4a730954e6d1b7441632
shawcorey <shaw.corey@gmail.com>
2021-07-27 12:32:24 -0500

    updated exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,3 +1,8 @@
 USE codeup_test_db;
 
-INSERT
+INSERT INTO albums (artist, Name, release_date, sales, genre)
+VALUES ('Micheal Jackson','Thriller', 1982, '47.9','Pop, post-disco, funk, rock'),
+('Whitney Houston', 'The Bodyguard', 1992, '32.4','R&B, soul, pop, soundtrack'),
+('Eagles','Their Greatest Hits (1971-1975)',1976,'41.2','Country rock, soft rock, folk rock');
+
+SELECT * FROM albums;
