938875993a06f4649cde00f2b977398031ad54d4
shawcorey <shaw.corey@gmail.com>
2021-07-30 19:29:49 -0500

    started limit exercises

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -1,11 +1,11 @@
-USE third_test_db;
+USE employees;
 
 SELECT CONCAT(first_Name, last_name)
-FROM third_test_db.employee
+FROM employees
 WHERE first_Name LIKE ('e%')
   AND last_Name LIKE ('%e');
 
 SELECT *
-FROM third_test_db.employee
+FROM employees
 WHERE MONTH(birth_date) = 12
   AND day(birth_date) = 25;
