4b919d45355f68a4536715cacb214c736a0aad1c
shawcorey <shaw.corey@gmail.com>
2021-08-02 14:28:12 -0500

    fininshed join_exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,10 +1,27 @@
-USE employees;
-
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01'
-  AND e.emp_no = 10001;
+use employees;
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+from dept_manager as dm
+         join employees e on e.emp_no = dm.emp_no
+         join departments d on d.dept_no = dm.dept_no
+where to_date = '9999-01-01'
+order by dept_name;
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+from dept_manager as dm
+         join employees e on e.emp_no = dm.emp_no
+         join departments d on d.dept_no = dm.dept_no
+where to_date = '9999-01-01'
+  and e.gender = 'F'
+order by dept_name;
+SELECT title, count(*) Total
+from titles as t
+         join employees e on e.emp_no = t.emp_no
+         join dept_emp de on e.emp_no = de.emp_no
+group by title;
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, salary
+from dept_manager as dm
+         join employees e on e.emp_no = dm.emp_no
+         join salaries s on e.emp_no = s.emp_no
+         join departments d on d.dept_no = dm.dept_no
+where dm.to_date = '9999-01-01'
+  and s.to_date = '9999-01-01'
+order by dept_name;
