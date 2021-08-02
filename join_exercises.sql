433f1b31a872591b66a8fcb3f6ff8b5c0c43230a
shawcorey <shaw.corey@gmail.com>
2021-08-02 15:38:14 -0500

    updated join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -25,3 +25,15 @@ from dept_manager as dm
 where dm.to_date = '9999-01-01'
   and s.to_date = '9999-01-01'
 order by dept_name;
+# The Bonus
+select CONCAT(e.first_name, ' ', e.last_name) AS Employee,
+       d.dept_name,
+       CONCAT(m.first_name, ' ', m.last_name) AS Manger
+from employees.employees e
+         join dept_emp de on e.emp_no = de.emp_no
+         join departments d on de.dept_no = d.dept_no
+         join dept_manager dm on de.dept_no = dm.dept_no
+         join employees m on dm.emp_no = m.emp_no
+where de.to_date = '9999-01-01'
+  and dm.to_date = '9999-01-01'
+order by dept_name, e.emp_no;
