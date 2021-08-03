2acd0a23d5f7f81a9f56447fef5e66343fa3dadb
shawcorey <shaw.corey@gmail.com>
2021-08-03 13:50:06 -0500

    created movie table

---
+++ movies_db_create_script.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS movies_db;
+
+USE movies_db;
+
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE IF NOT EXISTS movies (
+    title VARCHAR(50) NOT NULL,
+    rating INT UNSIGNED,
+    poster TEXT,
+    year INT NOT NULL,
+    genre VARCHAR(70) NOT NULL,
+    director VARCHAR(50) NOT NULL,
+    plot TEXT,
+    actors VARCHAR(100) NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
--- quotables_create_script.sql
+++ quotables_create_script.sql
@@ -1,13 +1,14 @@
 CREATE DATABASE IF NOT EXISTS quotables_db;
 
 USE quotables_db;
-
+DROP TABLE IF EXISTS quote_topic;
 DROP TABLE IF EXISTS quotes;
 DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS topics;
 #AUTHORS
 CREATE TABLE authors
 (
-    id                INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50),
     last_name  VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
@@ -18,21 +19,74 @@ VALUES ('Douglas', 'Adams'),
        ('Mark', 'Twain'),
        ('Kurt', 'Vonnergut');
 
-SELECT * FROM authors;
+SELECT *
+FROM authors;
+
+#TOPICS
+CREATE TABLE topics
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO topics(name)
+VALUES ('Space and Time'),
+       ('Humor'),
+       ('Office Life'),
+       ('Hitchiker''s Guide to the Galaxy');
+
+SELECT *
+FROM topics;
 
 #QUOTES
 CREATE TABLE quotes
 (
-    id                INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author_id INT UNSIGNED NOT NULL,
-    content           TEXT         NOT NULL,
+    content   TEXT         NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (author_id) REFERENCES authors (id)
 );
+#SEED QUOTES TABLE
+# CREATE TABLE quotes
+# (
+#     id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     content   TEXT         NOT NULL,
+#     author_id INT UNSIGNED NOT NULL,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (author_id) REFERENCES authors (id)
+# );
 
-SELECT * FROM quotes;
+# Seeds
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'I love deadlines. I love the whooshing noise they make as they go by.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'Time is an illusion. Lunchtime doubly so.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Mark' and last_name = 'Twain'),
+        'Clothes make the man. Naked people have little or no influence on society.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Kurt' and last_name = 'Vonnegut'),
+        'The universe is a big place, perhaps the biggest.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'), 'Don''t Panic.');
+
+SELECT *
+FROM quotes;
 
 
+#QUOTE_TOPIC
+
+CREATE TABLE quote_topic
+(
+    quote_id INTEGER UNSIGNED NOT NULL,
+    topic_id INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (quote_id) REFERENCES quotes (id),
+    FOREIGN KEY (topic_id) REFERENCES topics (id)
+);
 
 
 
